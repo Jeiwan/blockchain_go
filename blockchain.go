469281645c7f2e141001d83ee8c3cbdcf9764d85
Ivan Kuznetsov <me@jeiwan.ru>
2017-08-16 13:55:18 +0700

    Use NewBlock in Blockchain.AddBlock

--- blockchain.go
+++ blockchain.go
@@ -1,7 +1,5 @@
 package main
 
-import "time"
-
 // Blockchain keeps a sequence of Blocks
 type Blockchain struct {
 	blocks []*Block
@@ -10,8 +8,7 @@ type Blockchain struct {
 // AddBlock saves provided data as a block in the blockchain
 func (bc *Blockchain) AddBlock(data string) {
 	prevBlock := bc.blocks[len(bc.blocks)-1]
-	newBlock := &Block{time.Now().Unix(), []byte(data), prevBlock.Hash, []byte("")}
-	newBlock.SetHash()
+	newBlock := NewBlock(data, prevBlock.Hash)
 	bc.blocks = append(bc.blocks, newBlock)
 }
 
