6fbf7ac6b49e68bec550da3a41b32e460bfc9779
Ivan Kuznetsov <me@jeiwan.ru>
2017-08-16 14:20:18 +0700

    Make the output more verbose

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func main() {
 	bc.AddBlock("Send 2 more BTC to Ivan")
 
 	for _, block := range bc.blocks {
-		fmt.Printf("%s - %x\n", block.Data, block.Hash)
+		fmt.Printf("Prev. hash: %x\n", block.PrevBlock)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
 	}
 }
