8e6636983a6ce96cda005e07160fde22eef7becd
Ivan Kuznetsov <me@jeiwan.ru>
2017-09-03 09:57:32 +0700

    Fix printChain

--- cli.go
+++ cli.go
@@ -38,7 +38,6 @@ func (cli *CLI) printChain() {
 		block := bci.Next()
 
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
 		pow := NewProofOfWork(block)
 		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
