e3739acac9f4133a120aafdf134e9ab75bb7915d
Ivan Kuznetsov <me@jeiwan.ru>
2017-09-17 11:01:29 +0700

    Use the UTXO set to get balance

--- cli_getbalance.go
+++ cli_getbalance.go
@@ -10,12 +10,13 @@ func (cli *CLI) getBalance(address string) {
 		log.Panic("ERROR: Address is not valid")
 	}
 	bc := NewBlockchain()
+	UTXOSet := UTXOSet{bc}
 	defer bc.db.Close()
 
 	balance := 0
 	pubKeyHash := Base58Decode([]byte(address))
 	pubKeyHash = pubKeyHash[1 : len(pubKeyHash)-4]
-	UTXOs := bc.FindUTXO(pubKeyHash)
+	UTXOs := UTXOSet.FindUTXO(pubKeyHash)
 
 	for _, out := range UTXOs {
 		balance += out.Value
