8ea4bc859d45ecc07d7d40fbce6cad28fc721a29
Wei Yang <richard.weiyang@gmail.com>
2017-10-18 11:51:41 +0800

    Use View instead of Update in NewBlockchain()

--- blockchain.go
+++ blockchain.go
@@ -82,7 +82,7 @@ func NewBlockchain(nodeID string) *Blockchain {
 		log.Panic(err)
 	}
 
-	err = db.Update(func(tx *bolt.Tx) error {
+	err = db.View(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(blocksBucket))
 		tip = b.Get([]byte("l"))
 
